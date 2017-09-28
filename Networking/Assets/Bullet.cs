0fedac117726cec7f544c54bb0f78d601777bded
Rernokk <ssmith37@uccs.edu>
2017-09-28 02:39:22 -0600

    Committing Unity network project & Old C# Chatroom Project

---
+++ Networking/Assets/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+  private void OnCollisionEnter(Collision collision)
+  {
+    var hit = collision.gameObject;
+    var hitTarget = hit.GetComponent<Combat>();
+    if (hitTarget != null){
+      hitTarget.TakeDamage(10);
+      Destroy(gameObject);
+    }
+  }
+}
---
+++ Networking/Assets/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+  public GameObject bulletPrefab;
+
+	void Update () {
+    if (!isLocalPlayer)
+      return;
+
+    var x = Input.GetAxis("Horizontal") * .1f;
+    var z = Input.GetAxis("Vertical") * .1f;
+    transform.Translate(x, 0, z);
+
+    if (Input.GetKeyDown(KeyCode.Space)){
+      CmdFire();
+    }
+	}
+
+  [Command]
+  void CmdFire(){
+    var bullet = (GameObject)Instantiate(bulletPrefab, transform.position - transform.forward, Quaternion.identity);
+    bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+    NetworkServer.Spawn(bullet);
+    Destroy(bullet, 2f);
+  }
+
+  public override void OnStartLocalPlayer()
+  {
+    base.OnStartLocalPlayer();
+    GetComponent<MeshRenderer>().material.color = Color.cyan;
+  }
+}
